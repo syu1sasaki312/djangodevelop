1ae8faac7a2290acc803f915cf903e79538f0e16
syu1sasaki <syu1sasaki312@gmail.com>
2024-01-10 11:38:45 +0900

    no message

--- myapp/views/customer/register.py
+++ myapp/views/customer/register.py
@@ -6,7 +6,7 @@ class CustomerRegister(TemplateView):
     template_name = 'customer/register.html'
     def __init__(self):
         self.params = {
-            "info" : {"type":1, "message":"入力してください。"},
+            "info" : {"type" : 1, "message" : "入力してください。"},
             "form" : CustomerRegisterForm(),
         }
 
@@ -19,8 +19,8 @@ class CustomerRegister(TemplateView):
             self.params["form"].save(commit=True)
 
             self.params["form"] = CustomerRegisterForm()
-            self.params["info"] =  {"type":1, "message":"登録しました。"}
+            self.params["info"] =  {"type" : 1, "message" : "登録しました。"}
         else:
-            self.params["info"] =  {"type":9, "message":"入力エラーです。"}
+            self.params["info"] =  {"type" : 9, "message" : "入力エラーです。"}
 
         return render(request, self.template_name, context=self.params)
